2a09de38b3dd2af0fc310fb8f3b3b8e443e63184
Ferry Irawan <ferrwanz@gmail.com>
2021-04-21 01:57:21 +0700

    update config, add less support and antd framework, add unmet peer dependencies

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,59 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import {
-  BrowserRouter as Router, Switch, Route, Link,
-} from 'react-router-dom';
+import BrankasPv from '../components/pages/Brankas/BrankasPv';
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
+const el = document.getElementById('app');
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(<BrankasPv />, el);
 }
 
 render();
--- webpack.rules.js
+++ webpack.rules.js
@@ -1,16 +1,17 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = [
   // Add support for native node modules
   {
     test: /\.node$/,
-    use: "node-loader",
+    use: 'node-loader',
   },
   {
     test: /\.jsx?$/,
     use: {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
         exclude: /node_modules/,
-        presets: ["@babel/preset-react"],
       },
     },
   },
@@ -18,9 +19,9 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: "@marshallofsound/webpack-asset-relocator-loader",
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
       options: {
-        outputAssetBase: "native_modules",
+        outputAssetBase: 'native_modules',
       },
     },
   },
@@ -28,10 +29,22 @@ module.exports = [
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
     use: {
-      loader: "ts-loader",
+      loader: 'ts-loader',
       options: {
         transpileOnly: true,
       },
     },
+  },
+  {
+    test: /\.(le|c)ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      'css-loader',
+      'postcss-loader',
+      // Compiles Sass to CSS
+      'less-loader',
+    ],
   },
 ];
