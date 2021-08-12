abb2d1cb3f5496eb14d34b7287fae4b426f0d9ec
Ferry Irawan <ferrwanz@gmail.com>
2021-08-12 23:50:41 +0700

    update data and add note icon

--- src/components/organisms/SidebarOv/SidebarOv.tsx
+++ src/components/organisms/SidebarOv/SidebarOv.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeFilled, LockFilled } from '@ant-design/icons';
+import { FileProtectOutlined, HomeFilled, LockFilled } from '@ant-design/icons';
 
 const { Sider } = Layout;
 
@@ -28,7 +28,10 @@ const SidebarOv: React.VFC<EmptyObject> = () => (
         </Link>
       </Menu.Item>
       <Menu.Item key="notes">
-        <Link to="/notes">Notes</Link>
+        <Link to="/notes">
+          <FileProtectOutlined />
+          Notes
+        </Link>
       </Menu.Item>
     </Menu>
   </Sider>
